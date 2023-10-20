43f7e70f8673a49c4496c82f83211a35cff9fc19
Gavin Shaw <gshaw+github@pivotal.io>
2023-10-20 18:49:40 -0400

    Add GET endpoint to obtain die roll usage stats

--- main.go
+++ main.go
@@ -25,17 +25,46 @@ type DieRollResponse struct {
 	RollResults []int
 }
 
+type UsageStatsResponse struct {
+	TotalRequests int
+	GoodRequests  int
+	BadRequests   int
+	DieTypeUsage  map[int]int
+}
+
+var totalRequestCount int
+var goodRequestCount int
+var badRequestCount int
+
+var dieTypeMap map[int]int
+
 func main() {
+	dieTypeMap = make(map[int]int)
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/roll", diceRollHandler).Methods("POST")
+	router.HandleFunc("/stats", getUsageStatsHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+func getUsageStatsHandler(w http.ResponseWriter, r *http.Request) {
+	var response UsageStatsResponse
+
+	response.TotalRequests = totalRequestCount
+	response.GoodRequests = goodRequestCount
+	response.BadRequests = badRequestCount
+	response.DieTypeUsage = dieTypeMap
+
+	json.NewEncoder(w).Encode(response)
+}
+
 func diceRollHandler(w http.ResponseWriter, r *http.Request) {
+	totalRequestCount++
 	var rollList DieRollRequest
 	err := json.NewDecoder(r.Body).Decode(&rollList)
 	if err != nil {
+		badRequestCount++
 		fmt.Fprintf(w, "Bad request: %s", err)
 		return
 	}
@@ -52,10 +81,19 @@ func diceRollHandler(w http.ResponseWriter, r *http.Request) {
 			for rollNum := 0; rollNum < dieRoll.RollCount; rollNum++ {
 				resultingRoll[index].RollResults[rollNum] = rand.Intn(dieRoll.DieType) + 1
 			}
+
+			count, ok := dieTypeMap[dieRoll.DieType]
+			if ok {
+				dieTypeMap[dieRoll.DieType] = count + dieRoll.RollCount
+			} else {
+				dieTypeMap[dieRoll.DieType] = dieRoll.RollCount
+			}
 		}
+		goodRequestCount++
 		json.NewEncoder(w).Encode(resultingRoll)
 		return
 	}
 
+	badRequestCount++
 	fmt.Fprintf(w, "No die rolls specs found in request")
 }
