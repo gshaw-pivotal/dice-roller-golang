89fc3a7c05fe61a99f0fd87ba352d339ceaddf28
Gavin Shaw <gshaw+github@pivotal.io>
2023-10-20 14:26:06 -0400

    POST endpoint accepts requests to roll dies and returns a response with die rolls
    This would be the bare minimum program.

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type DieRollRequest struct {
+	Dies []DieRoll `json:"dies"`
+}
+
+type DieRoll struct {
+	DieType   int `json:"dieType"`
+	RollCount int `json:"rollCount"`
+}
+
+type DieRollResponse struct {
+	DieType     int
+	RollCount   int
+	RollResults []int
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -16,5 +33,29 @@ func main() {
 }
 
 func diceRollHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "You have reached the endpoint")
+	var rollList DieRollRequest
+	err := json.NewDecoder(r.Body).Decode(&rollList)
+	if err != nil {
+		fmt.Fprintf(w, "Bad request: %s", err)
+		return
+	}
+
+	if len(rollList.Dies) > 0 {
+		var resultingRoll = make([]DieRollResponse, len(rollList.Dies))
+		for index, dieRoll := range rollList.Dies {
+			resultingRoll[index] = DieRollResponse{
+				DieType:     dieRoll.DieType,
+				RollCount:   dieRoll.RollCount,
+				RollResults: make([]int, dieRoll.RollCount),
+			}
+
+			for rollNum := 0; rollNum < dieRoll.RollCount; rollNum++ {
+				resultingRoll[index].RollResults[rollNum] = rand.Intn(dieRoll.DieType) + 1
+			}
+		}
+		json.NewEncoder(w).Encode(resultingRoll)
+		return
+	}
+
+	fmt.Fprintf(w, "No die rolls specs found in request")
 }
